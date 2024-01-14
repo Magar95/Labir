e94d1d106abce0d234db7ea83ce07324095a0952
Magar95 <90345778+Magar95@users.noreply.github.com>
2024-01-14 16:55:44 +0300

    Update labirint.py
    Проект Лабиринт: версия 2
    Добавлено:
    ограничение движения по границам окна и стенам
    добавлены враги

--- labirint.py
+++ labirint.py
@@ -17,27 +17,52 @@ class Player(GameSprite):
         GameSprite.__init__(self, player_image, player_x, player_y, size_x, size_y)
         self.x_speed = player_x_speed
         self.y_speed = player_y_speed
-        
+
     def update(self):
-        self.rect.x += self.x_speed
-        self.rect.y += self.y_speed
+        if self.rect.x <= win_width-80 and self.x_speed > 0 or self.rect.x >= 0 and self.x_speed < 0: #
+            self.rect.x += self.x_speed #
+        platforms_touched = sprite.spritecollide(self, barriers, False) #
+        if self.x_speed > 0: #
+            for p in platforms_touched: #
+                self.rect.right = p.rect.left #
+        elif self.x_speed < 0: #
+            for p in platforms_touched: #
+                self.rect.left = p.rect.right #
+        if self.rect.y <= win_height-80 and self.y_speed > 0 or self.rect.y >= 0 and self.y_speed < 0: #
+            self.rect.y += self.y_speed #
+        platforms_touched = sprite.spritecollide(self, barriers, False) #
+        if self.y_speed > 0: #
+            for p in platforms_touched: #
+                self.rect.bottom = p.rect.top #
+        elif self.y_speed < 0: #
+            for p in platforms_touched: #
+                self.rect.top = p.rect.bottom #
 
 win_width = 700
 win_height = 500
 window = display.set_mode((win_width, win_height))
 display.set_caption("Лабиринт")
 back = (119, 210, 223)
+
+barriers = sprite.Group() #
+
 w1 = GameSprite('platform1.png',116, 250, 300, 50)
 w2 = GameSprite('platform2.png', 370, 100, 50, 400)
+
+barriers.add(w1) #
+barriers.add(w2) #
+
 packman = Player('hero.png', 5, 420, 80, 80, 0, 0)
 
+monster = GameSprite('cyborg.png', win_width - 80, 180, 80, 80) #
+final_sprite = GameSprite('pac-1.png', win_width - 85, win_height - 100, 80, 80) #
+finish = False #
 
 run = True
 while run:
     for e in event.get():
         if e.type == QUIT:
             run = False
-            
         elif e.type == KEYDOWN:
             if e.key == K_LEFT:
                 packman.x_speed = -5
@@ -47,7 +72,6 @@ while run:
                 packman.y_speed = -5
             elif e.key == K_DOWN:
                 packman.y_speed = 5
-
         elif e.type == KEYUP:
             if e.key == K_LEFT:
                 packman.x_speed = 0
@@ -57,12 +81,25 @@ while run:
                 packman.y_speed = 0
             elif e.key == K_DOWN:
                 packman.y_speed = 0
+                
+    if finish == False: #
+        window.fill(back) #
+        barriers.draw(window) #
+        monster.reset() #
+        final_sprite.reset() #
+        packman.reset() #
+        packman.update() #
+        if sprite.collide_rect(packman, monster): #
+            finish = True #
+            img = image.load('game-over_1.png') #
+            window.fill((255, 255, 255)) #
+            window.blit(transform.scale(img, (win_width, win_height)), (0, 0)) #
+        if sprite.collide_rect(packman, final_sprite): #
+            finish = True #
+            img = image.load('thumb.jpg') #
+            window.fill((255, 255, 255)) #
+            window.blit(transform.scale(img, (win_width, win_height)), (0, 0)) #
     
-    window.fill(back)
-    w1.reset()
-    w2.reset()
-    packman.reset()
-    packman.update()
-
     time.delay(25)
+    
     display.update()
